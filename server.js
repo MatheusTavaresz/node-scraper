0546002aa36e6e8ae4860cac68cd77b403ef8ba2
Matheus Patrick <mathtg2@gmail.com>
2023-09-26 14:38:41 -0300

    feat: add HTTPS request to fetch data

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const https = require('https');
 
 app.listen(port, () => {
     console.log(`Listening on ${port} port`)
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    // res.send('Hello')
+
+    const url = 'https://fbref.com/en/squads/18bb7c10/Arsenal-Stats'
+    https.get(url, res => {
+        let data = '';
+
+        res.on('data', chunk => data += chunk);
+        res.on('end', () => console.log(data))
+        
+    })
 })
