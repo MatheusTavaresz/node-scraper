6a00a2bc55fa1c20f2adb351aa7d6f62be44819c
Matheus Patrick <mathtg2@gmail.com>
2023-09-26 14:40:44 -0300

    build: jsdom config

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const https = require('https');
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom;
 
 app.listen(port, () => {
     console.log(`Listening on ${port} port`)
